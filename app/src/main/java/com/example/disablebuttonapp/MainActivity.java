8a2d9b73f53cad01cf3e33b2c709323ad5571407
Jonatan Kozlowski <kozlowskijonatan@gmail.com>
2024-09-15 18:23:42 +0200

    Final Version 1.0

--- app/src/main/java/com/example/disablebuttonapp/MainActivity.java
+++ app/src/main/java/com/example/disablebuttonapp/MainActivity.java
@@ -1,24 +1,59 @@
 package com.example.disablebuttonapp;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.disablebuttonapp.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonReset;
+    private Button buttonDisable;
+    private TextView textView;
+    private int clickCount = 0; // Zmienna do liczenia kliknięć
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        //Inicjalizacja przycisku i TextView
+        buttonReset = findViewById(R.id.buttonReset);
+        buttonDisable = findViewById(R.id.buttonDisable);
+        textView = findViewById(R.id.textView);
+
+        //Ustawienie początkowego tekstu w TextView
+        textView.setText("Liczba kliknięć: " + clickCount);
+
+        //Ustawienie nasłuchiwania na kliknięcie przycisku buttonDisable
+        buttonDisable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Zwiększ licznik kliknięć
+                clickCount++;
+
+                //Aktualizacja tekstu w TextView
+                textView.setText("Liczba kliknięć: " + clickCount);
+
+                //Po kliknięciu przycisk zostaje dezaktywowany
+                buttonDisable.setEnabled(false);
+            }
+        });
+
+        //Ustawienie nasłuchiwania na kliknięcie przycisku buttonReset
+        buttonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Aktualizacja tekstu w TextView
+                textView.setText("Liczba kliknięć: " + clickCount);
+
+                //Włączenie przycisku buttonDisable
+                buttonDisable.setEnabled(true);
+            }
         });
     }
 }
